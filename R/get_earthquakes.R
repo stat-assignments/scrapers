3604ffdc4726b89bd118dbd732c6479f39f6a1dc
Heike Hofmann <hhofmann4@unl.edu>
2025-04-06 08:15:54 -0500

    make sure the examples are automatically updating

--- R/get_earthquakes.R
+++ R/get_earthquakes.R
@@ -7,14 +7,17 @@
 #' @param base_url base url for the GET request
 #' @importFrom httr2 request req_headers req_url_query req_perform resp_body_string
 #' @importFrom utils read.csv
-#' @return data frame of earth quake records. Detailed information on each of the variables can be found as part of the [USGS ComCat Documentation](https://earthquake.usgs.gov/data/comcat/index.php)
+#' @importFrom lubridate today
+#' @return data frame of earth quake records. Detailed information on each of the variables can be found as part of the [USGS ComCat Documentation](https://earthquake.usgs.gov/data/comcat/index.php).
+#' By default, data for the last 30 days is requested from the archive. This is similar data to what USGS provides as `All Earthquakes` for the past 30 days in their spreadsheet/csv feed at https://earthquake.usgs.gov/earthquakes/feed/v1.0/csv.php
 #' @export
 #' @examples
 #' # example code
-#' one_day <- get_earthquakes(start_time = "2025-03-06", end_time = "2025-03-07")
+#' library(lubridate)
+#' todays <- get_earthquakes(start_time = today()-1, end_time = today())
 #' # all documented earthquakes with a magnitude of at least 9 on the Richter scale
-#' nines <-  get_earthquakes(start_time = "1800-01-01", end_time = "2025-03-07", min_magnitude=9)
-get_earthquakes <- function(start_time, end_time=NULL, min_magnitude=NULL, base_url="https://earthquake.usgs.gov/fdsnws/event/1/query") {
+#' nines <-  get_earthquakes(start_time = "1800-01-01", end_time = today(), min_magnitude=9)
+get_earthquakes <- function(start_time = today()-30, end_time=NULL, min_magnitude=NULL, base_url="https://earthquake.usgs.gov/fdsnws/event/1/query") {
   #library(xml2)
   #url <- "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02"
   #library(httr2)
